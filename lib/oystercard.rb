f722c2d227219bea47851685845ff6bf87235f05
liskowsky <karollebiedzinski@gmail.com>
2016-07-12 17:22:42 +0100

    Adds touch in/out feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,29 +1,34 @@
-
-
-
 class Oystercard
 
-MAX_LIMIT = 90
-
-  attr_accessor :balance
+  MAX_LIMIT = 90
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     fail "Oystercard's limit reached" if exceeds_limit?(amount)
-    self.balance = amount + balance
+    @balance = amount + @balance
   end
 
   def deduct(amount)
-    self.balance = balance - amount
+    @balance = @balance - amount
   end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  private
 
   def exceeds_limit?(amount)
-    self.balance + amount > MAX_LIMIT
+    @balance + amount > MAX_LIMIT
   end
-
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,17 +11,28 @@ describe Oystercard do
     subject.top_up 50
     expect(subject.balance).to eq 50
   end
-
   it "deducts money from current balance" do
     subject.top_up 20
-      expect{subject.deduct 10}.to change{subject.balance}.by -10
-
-    end
-
+    expect{ subject.deduct 10 }.to change{ subject.balance }.by -10
+  end
   it "error emerges when oystercard exceeds 90 pounds" do
     subject.top_up( described_class::MAX_LIMIT )
-    expect{subject.top_up 10}.to raise_error "Oystercard's limit reached"
-
+    expect{ subject.top_up 10 }.to raise_error "Oystercard's limit reached"
   end
+
+  describe "#Card usage" do
+    it { is_expected.to respond_to :touch_in }
+    it { is_expected.to respond_to :touch_out }
+    it "in journey when touched in" do
+      subject.touch_in
+      expect(subject.in_journey).to eq true
+    end
+    it "not in journey when touched out" do
+      subject.touch_out
+      expect(subject.in_journey).to eq false
+    end
+  end
+
+
 
 end
